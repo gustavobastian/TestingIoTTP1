45152530c9af569fa01ed4e1c5a74e8bbc99e3c0
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-10-28 18:04:09 -0300

    borrando elementos... refactoring

--- src/lista.js
+++ src/lista.js
@@ -1,25 +1,21 @@
 module.exports = class Lista{
+    
     constructor(){          
         this.ArregloDeElementos=[];
     };
 
     count(){        
-        return this.ArregloDeElementos.length;
+        return  this.ArregloDeElementos.length;
     }
 
-    add(clave,valor){
-        if(((clave==null) || (valor==null))){
+    async add(clave,valor){
+        if(((clave==null) || (valor==null))||(typeof(clave)!='string')||(typeof(valor)!='string')){
             return false;
-        }
-        else if((typeof(clave)!='string')||(typeof(valor)!='string')){
-            return false;
-        }                       
-
-        let busqueda= this.find(clave);
-        
+        }       
+        let busqueda= await this.find(clave);        
         if(busqueda==null){
-            let element=[clave,valor];
-            this.ArregloDeElementos.push(element);
+            let element={'clave':clave,'valor':valor};
+            this.ArregloDeElementos.push(element);        
             return true;
         }
         else{
@@ -27,17 +23,37 @@ module.exports = class Lista{
         }
     }
 
-    find(clave){
-        let value=null;
-        this.ArregloDeElementos.forEach(element => {            
-            if(element[0]==clave){          
-                value=element[1]
+    async find(clave){
+        let value=null;        
+        await this.ArregloDeElementos.forEach(element => {            
+            if(element.clave==clave){          
+                value=element.valor;
             }
         });
         return value;
     }
 
-    delete(clave){
-        return false;
+    async delete(clave){                
+        let resultado= await this.find(clave);        
+        if(resultado==null){ 
+           // console.log("len:",this.ArregloDeElementos.length);           
+            return false;
+        }
+        else{
+            let indice=await this.indexOf(clave);                        
+            this.ArregloDeElementos.splice(indice,1)
+            return true;
+        }
+
+    }
+
+    indexOf(clave){
+        let value=null;        
+            for (let i = 0; i < this.ArregloDeElementos.length; i++) {
+                if (clave == this.ArregloDeElementos[i].clave) {
+                    return value = i;
+                }
+            };        
+
     }
 };
--- test/lista.js
+++ test/lista.js
@@ -11,8 +11,8 @@ Pruebas a realizar
 * En una lista con tres o mas elementos, agregar una clave menor que todas las existentes, comprobar que la lista de claves se obtiene siempre ordenada
 * En una lista con tres o mas elementos, agregar una clave mayor que todas las existentes, comprobar que la lista de claves se obtiene siempre ordenada
 * En una lista con tres o mas elementos, agregar una clave menor intermedia entre las existentes, comprobar que la lista de claves se obtiene siempre ordenada
-* En una lista con elementos, borrar una clave y comprobar que al buscarla no existe
-* En una lista con elementos, comprobar el error al borrar una clave que no existe
+
+
 
 
 
@@ -21,7 +21,7 @@ Pruebas a realizar
 const assert = require("chai").assert;
 const ListaN = require("../src/lista");
 
-describe("en una lista vacia", ()=>{
+describe("en una lista vacia",async ()=>{
 
     var Lista = new ListaN();
     /*En una lista vacia, comprobar que hay cero elementos almacenados  */
@@ -31,7 +31,7 @@ describe("en una lista vacia", ()=>{
     /*
     * En una lista vacia, comprobar el error al borrar una clave
     */
-    var resultado=Lista.delete("uno");
+    var resultado=await Lista.delete("uno");    
     it("No se puede borrar en lista vacia", ()=>{
         assert.equal(resultado,false)
     })
@@ -41,8 +41,9 @@ describe("en una lista vacia", ()=>{
 describe("agrego elementos a una lista vacia", ()=>{
     /*En una lista vacía, al agregar un elemento, comprobar que hay un elemento almacenados*/
     var Lista = new ListaN();
-    it("agrego elemento", ()=>{        
-        assert.equal(Lista.add("clave","valor"),true)
+    it("agrego elemento", async ()=>{        
+        let resultado=await Lista.add("clave","valor");
+        assert.equal(resultado,true)
         assert.equal(Lista.count(),1)
     });
   
@@ -53,36 +54,43 @@ describe("agrego elementos a una lista vacia", ()=>{
     * En una lista con tres o mas elementos, consultar el valor de la ultima clave y comprobar que coinciden
     * En una lista con tres o mas elementos, consultar el valor de una clave intermdia y comprobar que coinciden
     * */
-    it("se puede recuperar el valor a partir de una clave",()=>{        
-        Lista.add("clave","valor");
-        assert.equal(Lista.find("clave"),"valor");
+    it("se puede recuperar el valor a partir de una clave", async ()=>{        
+        await Lista.add("clave","valor");
+        let resultado=await Lista.find("clave");
+        assert.equal(resultado,"valor");
     })
     /*
     * En una lista con elementos, comprobar el error al buscar una clave que no existe   
     Retorno null... no esta definido 
     */
-    it("se presenta error si no existe la clave",()=>{                
-        var resultado=Lista.find(("clave2"),"valor");
+    it("se presenta error si no existe la clave",async ()=>{                
+        var resultado=await Lista.find(("clave2"),"valor");
         assert.equal(resultado,null)
     })
 
 })
 
 describe("agrego 2 elementos a una lista vacia", ()=>{
-    var Lista = new ListaN();
-    it("agrego 2 elementos", ()=>{        
-        assert.equal(Lista.add("uno","1"),true)
-        assert.equal(Lista.add("dos","2"),true)
-        assert.equal(Lista.count(),2)
+    var ListaLocal = new ListaN();
+    it("agrego 2 elementos", async ()=>{        
+        var element1=await ListaLocal.add("uno","1");
+        var element2=await ListaLocal.add("dos","2");
+        assert.equal(element1,true)
+        assert.equal(element2,true)
+        assert.equal(ListaLocal.count(),2)
     });    
     /* En una lista vacia, agregar una clave con un valor conocido, consultar el valor asociado a la clave y comprobar que coinciden*/
-    it("se puede recuperar el valor a partir de una clave",()=>{  
-        assert.equal(Lista.find("uno"),"1");
-        assert.equal(Lista.find("dos"),"2");
+    it("se puede recuperar el valor a partir de una clave",async()=>{  
+        var resultado1=await ListaLocal.find("uno");
+        var resultado2=await ListaLocal.find("dos");
+        assert.equal(resultado1,"1");
+        assert.equal(resultado2,"2");
     })
-})
 
-describe("agrego elementos con clave duplicada", ()=>{ 
+    
+
+})
+describe("agrego elementos con clave duplicada", async ()=>{ 
     /*
     * En una lista con elementos, comprobar el error al agregar una clave duplicada con la menor clave preexistente
     * En una lista con elementos, comprobar el error al agregar una clave duplicada con la mayor clave preexistente
@@ -90,10 +98,10 @@ describe("agrego elementos con clave duplicada", ()=>{
     */
 
     var Lista = new ListaN();
-    Lista.add("uno","1");
-    Lista.add("dos","2");
+    await Lista.add("uno","1");
+    await Lista.add("dos","2");
             
-    var resultado=Lista.add("uno","2");
+    var resultado=await Lista.add("uno","2");
     it("la funcion devuelve error",()=>{
         assert.isFalse(resultado);
     })
@@ -101,7 +109,7 @@ describe("agrego elementos con clave duplicada", ()=>{
         assert.equal(Lista.count(),2)
     })        
 
-    var resultado2=Lista.add("dos","2");
+    var resultado2=await Lista.add("dos","2");
     it("la funcion devuelve error en el caso 2",()=>{
         assert.isFalse(resultado2);
     })
@@ -110,10 +118,10 @@ describe("agrego elementos con clave duplicada", ()=>{
     })        
 })  
 
-describe("agrego elementos con valores erroneos", ()=>{
+describe("agrego elementos con valores erroneos", async ()=>{
     /*En una lista vacia, comprobar el error al agregar elementos con claves vacias o que no sean cadenas*/
     var Lista = new ListaN();
-    var resultado = Lista.add(1,1);
+    var resultado = await Lista.add(1,1);
     it("la funcion devuelve error",()=>{
         assert.isFalse(resultado);
     })
@@ -123,3 +131,39 @@ describe("agrego elementos con valores erroneos", ()=>{
 })  
 
 
+
+describe("elimino elementos a una lista ",async ()=>{
+    var ListaLocal = new ListaN();
+    await ListaLocal.add("uno","1");
+    await ListaLocal.add("dos","2");
+    /*
+     En una lista con elementos, comprobar el error al borrar una clave que no existe
+     */
+     
+     let resultado1=await ListaLocal.delete("seis");
+     it("No se puede borrar una clave que no existe", async ()=>{        
+         assert.equal(resultado1,false)
+     })
+     let resultado5 = await ListaLocal.count();
+     it("y no cambia la cantidad de elementos almacenados",async ()=>{                  
+             assert.equal(resultado5, 2);
+         }) 
+
+     /*
+     * En una lista con elementos, borrar una clave y comprobar que al buscarla no existe
+     */
+     var resultado2= await ListaLocal.delete("uno");
+     it("Se puede borrar una clave que existe", ()=>{
+         assert.equal(resultado2,true)
+     })
+
+     let resultado6= await ListaLocal.count();
+     it("y cambia la cantidad de elementos almacenados",async ()=>{
+        assert.equal(resultado6,1);
+     }) 
+
+     let resultado3=await ListaLocal.find("uno");
+     it("y no se puede recuperar el elemento eliminado",async ()=>{
+        assert.equal(resultado3,null);
+     }) 
+})
